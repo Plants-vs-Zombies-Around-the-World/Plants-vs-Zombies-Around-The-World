483174e4f4c83f334bbb66d7070336e9da0f46b4
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2021-01-12 12:15:40 +0800

    you can now delete profiles yaaaaay

--- Start.gd
+++ Start.gd
@@ -125,9 +125,8 @@ func _on_MakeProfileButton_pressed():
 	file.close()
 	save_data = json_data.result
 	
-	# this is for when a profile has been created
 	playerName = $NewProfileMaker/Control/ProfileMakerInput.get_text()
-	if isSaving == true:
+	if isSaving == true: # this is for when a profile has been created
 			if playerName.length() > 0:
 				# do stuff for save file
 				save_data["profileNum"] = save_data["data"].size()-save_data["data"].count(null)
@@ -163,7 +162,7 @@ func _on_MakeProfileButton_pressed():
 				$NewProfileMaker/Control/ProfileMakerWarning/Label.text = ""
 			else:
 				$NewProfileMaker/Control/ProfileMakerWarning/Label.text = "Name cannot be blank"
-	elif isSaving == false:
+	elif isSaving == false: # this is for when a profile is edited
 		print("yeet")
 		playerName = $NewProfileMaker/Control/ProfileMakerInput.text
 		if playerName.length() > 0:
@@ -663,3 +662,98 @@ func _on_ProfileEditButton_pressed():
 	$ProfilePanel/Control/ProfileList/Profile4/Button.pressed = false
 	
 	$NewProfileMaker.show()
+	$ProfilePanel.hide()
+
+func _on_ProfileDeleteButton_pressed():
+	#see what button got pressed
+	if $ProfilePanel/Control/ProfileList/Profile0/Button.pressed == true:
+		pressedButton = 0
+	if $ProfilePanel/Control/ProfileList/Profile1/Button.pressed == true:
+		pressedButton = 1
+	if $ProfilePanel/Control/ProfileList/Profile2/Button.pressed == true:
+		pressedButton = 2
+	if $ProfilePanel/Control/ProfileList/Profile3/Button.pressed == true:
+		pressedButton = 3
+	if $ProfilePanel/Control/ProfileList/Profile4/Button.pressed == true:
+		pressedButton = 4
+	
+	# unpress all profiles
+	$ProfilePanel/Control/ProfileList/Profile0/Button.pressed = false
+	$ProfilePanel/Control/ProfileList/Profile1/Button.pressed = false
+	$ProfilePanel/Control/ProfileList/Profile2/Button.pressed = false
+	$ProfilePanel/Control/ProfileList/Profile3/Button.pressed = false
+	$ProfilePanel/Control/ProfileList/Profile4/Button.pressed = false
+	
+	$DeleteConfirmation.show()
+	$ProfilePanel.hide()
+
+func _on_DeleteProfileNoButton_pressed():
+	$DeleteConfirmation.hide()
+	$ProfilePanel.show()
+
+func _on_DeleteProfileYesButton_pressed():
+	# read save file
+	file.open("res://save_data.json", File.READ)
+	json_data = JSON.parse(file.get_as_text())
+	file.close()
+	save_data = json_data.result
+	
+	# edit save file and extract stuff from it
+	save_data["data"][pressedButton] = null
+	save_data["data"].sort_custom(customProfileSorter, "sort")
+	profileNum = save_data["profileNum"]
+	
+	if save_data["data"][profileNum] == null && (save_data["data"].size() - save_data["data"].count(null)) > 0: # do stuff normally if there are some profiles left
+		print("many profiles left")
+		profileNum -= 1
+		save_data["profileNum"] = profileNum
+		
+		# update profile list when new profile is made
+		for n in range(5):
+			if n < save_data["data"].size() - save_data["data"].count(null):
+				# for available profiles
+				get_node("ProfilePanel/Control/ProfileList/Profile"+str(n)+"/Button").text = save_data["data"][n]["name"]
+				get_node("ProfilePanel/Control/ProfileList/Profile"+str(n)+"/Button").disabled = false
+			else:
+				# for unavailable profiles
+				get_node("ProfilePanel/Control/ProfileList/Profile"+str(n)+"/Button").disabled = true
+				match n:
+					0:
+						get_node("ProfilePanel/Control/ProfileList/Profile"+str(n)+"/Button").text = "(Player One)"
+					1:
+						get_node("ProfilePanel/Control/ProfileList/Profile"+str(n)+"/Button").text = "(Player Two)"
+					2:
+						get_node("ProfilePanel/Control/ProfileList/Profile"+str(n)+"/Button").text = "(Player Three)"
+					3:
+						get_node("ProfilePanel/Control/ProfileList/Profile"+str(n)+"/Button").text = "(Player Four)"
+					4:
+						get_node("ProfilePanel/Control/ProfileList/Profile"+str(n)+"/Button").text = "(Player Five)"
+		$ChangeProfile/ProfileName/Label.text = save_data["data"][profileNum]["name"]
+		
+		# overwrite save file
+		var editedSave = File.new()
+		editedSave.open("res://save_data.json", File.WRITE)
+		editedSave.store_line(JSON.print(save_data))
+		editedSave.close()
+		
+		# finishing touches
+		$DeleteConfirmation.hide()
+		$ProfilePanel.show()
+	elif save_data["data"][profileNum] == null && (save_data["data"].size() - save_data["data"].count(null)) == 0: # if there are no profiles left make a new one
+		print("no profiles left")
+		
+		# overwrite save file
+		var editedSave = File.new()
+		editedSave.open("res://save_data.json", File.WRITE)
+		editedSave.store_line(JSON.print(save_data))
+		editedSave.close()
+		
+		$DeleteConfirmation.hide()
+		$ProfilePanel.hide()
+		_ready()
+	
+class customProfileSorter: # this is for sorting profiles after one gets deleted
+	static func sort(a, b):
+		if a != null && b == null:
+			return true
+		return false
