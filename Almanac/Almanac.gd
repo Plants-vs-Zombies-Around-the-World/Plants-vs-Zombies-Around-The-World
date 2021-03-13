26c23d88bbdced8bae4f62bdca1252f093151a02
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2021-03-13 18:58:19 +0800

    yay more plant stuff

--- Almanac/Almanac.gd
+++ Almanac/Almanac.gd
@@ -1,4 +1,33 @@
 extends Node2D
 
+var profileNum
+
+var plantText
+var plantName
+var plantStats
+
+var zombieText
+var zombieName
+var zombieStats
+
 func _ready():
-	pass # Replace with function body.
+	# get the save file
+	var fileSave = File.new()
+	fileSave.open("res://save_data.json", File.READ)
+	var saveData = fileSave.get_as_text()
+	saveData = JSON.parse(saveData)
+	fileSave.close()
+	
+	# get the almanac file
+	var fileAlmanac = File.new()
+	fileAlmanac.open("res://Almanac/almanac.json", File.READ)
+	var almanacData = fileAlmanac.get_as_text()
+	almanacData = JSON.parse(almanacData)
+	fileAlmanac.close()
+	
+	# update list of plants in the almanac
+	for n in almanacData.result["plants"].size():
+		pass
+		
+	# update almanac text when you click on something
+	
