d686f3cb9f83cd3725f9882db21e40794beb338f
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2021-08-08 22:54:34 +0800

    peashooter animations

--- Features/plants/peashooter.gd
+++ Features/plants/peashooter.gd
@@ -4,8 +4,11 @@ var health
 var damageOutput
 
 func _ready():
+	$Rigger/AnimationPlayerIdle.play("default")
+	$Rigger/AnimationPlayerBlink.play("blink")
 	health = 120
 	damageOutput = 20
+	
 	
 func _process(delta):
 	if health == 0:
