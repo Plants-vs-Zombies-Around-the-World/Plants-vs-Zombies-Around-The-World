df1a53be379492d4d6ddd857856bec4b0fbc5641
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2021-08-09 10:55:28 +0800

    improved peashooter blinking

--- Features/plants/peashooter.gd
+++ Features/plants/peashooter.gd
@@ -5,11 +5,13 @@ var damageOutput
 
 func _ready():
 	$Rigger/AnimationPlayerIdle.play("default")
-	$Rigger/AnimationPlayerBlink.play("blink")
 	health = 120
 	damageOutput = 20
-	
 	
 func _process(delta):
 	if health == 0:
 		print("oh. i'm die. thank you forever.")
+
+
+func _on_BlinkTimer_timeout():
+	$Rigger/AnimationPlayerBlink.play("blink")
